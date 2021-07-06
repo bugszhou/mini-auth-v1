1c3e358d116d0bd14bfd2b27101b9a2f4bd302b2
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-07-06 10:42:02 +0800

    feat: 增加参数类型

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -52,25 +52,35 @@ interface IMiniAuthOptions {
    */
   headers?: {
     [key: string]: string;
-  }
+  };
 }
 
 interface IMiniAuthGetTokenOptions {
   /**
    * 是否强制刷新，默认值为false
-   * 
+   *
    * true - 先删除缓存数据，然后重新获取js-code，请求服务端获取最新数据
-   * 
+   *
    * false - 从缓存获取数据，如果缓存中的数据过期了，将执行值为true的步骤
    */
-  isRefresh: boolean;
+  isRefresh?: boolean;
+  /**
+   * 支付宝小程序专用，用于设置授权类型
+   *
+   * auth_base - 静默授权，不会发起授权浮窗。在支付宝客户端获取 auth_code，传入服务端调用 alipay.system.oauth.token 接口获取支付宝会员标识（user_id）
+   *
+   * auth_user - 主动授权，需要用户手动点击同意，用户同意后，用于 网站支付宝登录 和 获取用户信息（现已升级为“获取会员基础信息”，且新功能包不再使用 alipay.user.info.share 接口）功能。
+   *
+   * auth_zhima - 用户芝麻信息。
+   */
+  scopes?: string[];
 }
 
 interface IGetTokenReturn<IReturnData> {
   /**
    * 缓存的数据
    */
-  data: IReturnData
+  data: IReturnData;
   /**
    * 缓存有效时间，单位毫秒
    * 默认：6800000毫秒
@@ -83,10 +93,14 @@ interface IGetTokenReturn<IReturnData> {
 }
 
 declare module "mini-auth-v1" {
-  export function creatMiniAuth<IReturnData>(options: IMiniAuthOptions): MiniAuth<IReturnData>;
+  export function creatMiniAuth<IReturnData>(
+    options: IMiniAuthOptions,
+  ): MiniAuth<IReturnData>;
   /**
    * 获取Token数据，遇到网络问题请求失败，会重试3次
    * @param opts - object
    */
-  export const getToken: <IReturnData>(opts?: IMiniAuthGetTokenOptions) => Promise<IGetTokenReturn<IReturnData>>;
+  export const getToken: <IReturnData>(
+    opts?: IMiniAuthGetTokenOptions,
+  ) => Promise<IGetTokenReturn<IReturnData>>;
 }
